f0991edafbcc52cd9d08ecd3a20d717d0269e3d4
Patrick Gillan <pgillan@minorimpact.com>
2020-07-06 21:30:06 -0400

    Turn off leds when the keyboard stars up.

--- keyboards/massdrop/ctrl/keymaps/pgillan145/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/pgillan145/keymap.c
@@ -49,6 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
+    // Disable the LEDs when the keyboard boots up
+    rgb_matrix_disable_noeeprom();
 };
 
 // Runs constantly in the background, in a loop.
